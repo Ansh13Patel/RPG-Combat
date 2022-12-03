37e206db26a572612236ca66cd6535789b8fdf4b
Ansh13Patel <66872036+Ansh13Patel@users.noreply.github.com>
2022-12-03 15:53:00 +0530

    Project Creation
    Create a new C++ project in Unreal Engine.

---
+++ RPGCombat/Intermediate/Build/Win64/RPGCombatEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.34.31935
+#include "D:/My Games/RPG-Combat/RPGCombat/Intermediate/Build/Win64/RPGCombatEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ RPGCombat/Source/RPGCombat/RPGCombatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPGCombatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGCOMBAT_API ARPGCombatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
